d10d0b0fb8fa99a79e31896cee06d84f33bcf04c
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-01-08 14:32:22 +0100

    add order input validation middleware to endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import ordersController from '../modules/orders/ordersController';
+import validations from '../middlewares/validations';
 
 const router = express.Router();
 
-router.post('/orders', ordersController.create);
+router.post('/orders', validations.createOrderInputValidation, ordersController.create);
 router.put('/orders/:id', ordersController.update);
 
 export default router;
