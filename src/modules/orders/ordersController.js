b7a2492f6dddea804c87adc4191fbada7c17abcc
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-01-08 09:27:00 +0100

    create update enpoint for order

--- src/modules/orders/ordersController.js
+++ src/modules/orders/ordersController.js
@@ -10,6 +10,16 @@ const create = async (req, res) => {
   }
 }
 
+const update = async (req, res) => {
+  try {
+    const result = await orderService.updateOrder(req.params.id, req.body);
+    return response.success(res, result);
+  } catch (error) {
+    return response.error(res);
+  }
+}
+
 export default {
-  create
+  create,
+  update
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ import ordersController from '../modules/orders/ordersController';
 const router = express.Router();
 
 router.post('/orders', ordersController.create);
+router.put('/orders/:id', ordersController.update);
 
 export default router;
