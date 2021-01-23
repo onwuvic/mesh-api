89aee222d9e3c3294390d3ba71b4aa90590a5ac0
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-01-23 12:28:26 +0100

    add environment variables

---
+++ src/config/firebase.js
@@ -0,0 +1,25 @@
+import * as admin from "firebase-admin";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+  projectType: process.env.PROJECT_TYPE,
+  projectId: process.env.PROJECT_ID,
+  privateKeyId: process.env.PRIVATE_KEY_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  clientEmail: process.env.CLIENT_EMAIL,
+  clientId: process.env.CLIENT_ID,
+  authURI: process.env.AUTH_URI,
+  tokenURI: process.env.TOKEN_URI,
+  authProviderCertURL: process.env.AUTH_PROVIDER_CERT_URL,
+  clientCertURL: process.env.CLIENT_CERT_URL
+};
+
+export const firebase = admin.initializeApp({
+  credential: admin.credential.cert(config),
+  databaseURL: process.env.DATABASE_URL
+})
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
